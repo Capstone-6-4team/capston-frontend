5a0665e01c8315cb3ffe13d493b8436fef21d087
taehun97 <zaqxsw1397@gmail.com>
2022-05-17 19:16:08 +0900

    feat: GuestHouse & Room Registration Complete

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import { getToken } from './api/AuthAPI'
 import PublicRoute from './route/PublicRoute'
 import PrivateRoute from './route/PrivateRoute'
 import Signup from './pages/auth/SignUp'
+import HouseRegister from './pages/room/HouseRegister'
+import RoomRegister from './pages/room/RoomRegister';
+import BedLocationTest from './pages/room/BedLocationTest';
 import { Routes } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil'
 
 function App() {
 
@@ -34,9 +38,12 @@ function App() {
     //     </a>
     //   </header>
     // </div>
-    <>
+    <RecoilRoot>
       <PublicRoute component={Signup} path="/signup" />
-    </>
+      <PublicRoute component={HouseRegister} path="/house/register" />
+      <PublicRoute component={RoomRegister} path="/house/:houseId/room/register" />
+      <PublicRoute component={BedLocationTest} path="/bed" />
+    </RecoilRoot>
   );
 }
 
